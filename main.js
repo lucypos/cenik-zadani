74a4791fcfa1af4ec271ae3333f0281cd6951036
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-08 20:52:34 +0100

    rozpracované

---
+++ main.js
@@ -0,0 +1,10 @@
+const selectPlan = (planNumber) => {
+    if (let planNumber = document.getElementById('plan1')) {
+        return plan1.classList.add('plan--selected')
+    }
+    if (planNumber = plan2) {
+        if (planNumber === document.getElementById('plan2')) {
+            return plan2.classList.add('plan--selected')
+        }
+}
+}

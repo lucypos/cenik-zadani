5f1fcd630cba33005db6f74849988f3fe99a2992
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-09 10:53:34 +0100

    dú2

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const selectPlan = (planNumber) => {
-    if (let planNumber = document.getElementById('plan1')) {
-        return plan1.classList.add('plan--selected')
+    if (planNumber === plan1 ) {
+    return plan1.classList.add('plan--selected')
     }
-    if (planNumber = plan2) {
-        if (planNumber === document.getElementById('plan2')) {
-            return plan2.classList.add('plan--selected')
+    if (planNumber === plan2 ) {
+        return plan2.classList.add('plan--selected')
         }
 }
-}
+
+let plan1 =1
+let plan2 = 2
+let plan3 = 3

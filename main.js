387ccbe808fd1cfb8803a5dd115284b1f6a5c900
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-09 13:18:24 +0100

    dú-final

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const selectPlan = (planNumber) => {
-    if (planNumber === plan1 ) {
-    return plan1.classList.add('plan--selected')
+    document.getElementById('plan1').classList.remove('plan--selected')
+    document.getElementById('plan2').classList.remove('plan--selected')
+    document.getElementById('plan3').classList.remove('plan--selected')
+    if (planNumber === plan1) {
+    return document.getElementById('plan1').classList.add('plan--selected')
     }
     if (planNumber === plan2 ) {
-        return plan2.classList.add('plan--selected')
+        return document.getElementById('plan2').classList.add('plan--selected')
         }
+    if (planNumber === plan3 ) {
+        return document.getElementById('plan3').classList.add('plan--selected')
+        }
+        
 }
 
-let plan1 =1
+let plan1 = 1
 let plan2 = 2
 let plan3 = 3
